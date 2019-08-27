d1d40c6a1ce4e584cdfe149698da9bea5d1669e7
campostalita <campos.talita@hotmail.com>
2019-08-27 15:57:18 -0300

    adicionando código JS ok e começo de CSS

--- src/cipher.js
+++ src/cipher.js
@@ -3,41 +3,40 @@ window.cipher = {
   decode: decode
 };
           
-function encode(offset,string){
+function encode(offset, string) {
   let textoCodificado = [];
   let size = string.length;   
-  for(let i = 0; i <size; i++){
+  for (let i = 0; i <size; i++) {
     const transformAsc = string[i].charCodeAt(0);
-  if(transformAsc >= 65 && transformAsc <= 90){
-    const numerador =((transformAsc - 65 + offset) % 26) + 65;
-    const sCharCode = String.fromCharCode(numerador);
+    if (transformAsc >= 65 && transformAsc <= 90) {
+      const numerador =((transformAsc - 65 + offset) % 26) + 65;
+      const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
-  } else if (transformAsc >= 97 && transformAsc <= 122) {
-    const numerador = ((transformAsc - 97 + offset) % 26) + 97;
-    const sCharCode = String.fromCharCode(numerador);
+    } else if (transformAsc >= 97 && transformAsc <= 122) {
+      const numerador = ((transformAsc - 97 + offset) % 26) + 97;
+      const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
-  }else{
-      string.push(string(i));
+    } else {
+      textoCodificado.push(string[i]);
     }
   }
   return textoCodificado.join("");  
 }
 
-
-function decode(offset,string){
+function decode(offset, string) {
   let textoCodificado = [];
   let size = string.length;
-  for(let i = 0; i <size; i++){
+  for (let i = 0; i <size; i++) {
     const transformAsc = string[i].charCodeAt(0);
-  if(transformAsc >= 65 && transformAsc <= 90){
-    const numerador =((transformAsc - 65 - offset) % 26) + 65;
-    const sCharCode = String.fromCharCode(numerador);
+    if (transformAsc >= 65 && transformAsc <= 90) {
+      const numerador =((transformAsc - 90 - offset) % 26) + 90;
+      const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
-  } else if (transformAsc >= 97 && transformAsc <= 122) {
-    const numerador = ((transformAsc - 97 - offset) % 26) + 97;
-    const sCharCode = String.fromCharCode(numerador);
+    } else if (transformAsc >= 97 && transformAsc <= 122) {
+      const numerador = ((transformAsc - 122 - offset) % 26) + 122;
+      const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
-  }else{
+    } else {
       string.push(string(i));
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+document.getElementById("cliqueCode").addEventListener("click", clique);
+document.getElementById("cliqueDecode").addEventListener("click", cliqueDecode);
+
 //import { Cipher } from "crypto";
 
-function clique(){
-    event.preventDefault();   
-    let string = document.getElementById("string").value;
-    let offset = parseInt(document.getElementById("offset").value);
-    let result = encode(offset,string); //chamando a função do cipher.js
-    document.getElementById("msgCodificada").innerHTML = `Sua mensagem codificada é ${result}`;
- }
+function clique() {
+  event.preventDefault();   
+  let string = document.getElementById("string").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  let result = window.cipher.encode(offset, string); //chamando a função do cipher.js
+  document.getElementById("msgCodificada").innerHTML = `Sua mensagem codificada é ${result}`;
+}
 
-function cliqueDecode(){
-    event.preventDefault();   
-    let string = document.getElementById("stringDecode").value;
-    let offset = parseInt(document.getElementById("offsetDecode").value);
-    let result = decode(offset,string); //chamando a função do cipher.js
-    document.getElementById("msgDecodificada").innerHTML = `Sua mensagem decodificada é ${result}`; 
+function cliqueDecode() {
+  event.preventDefault();   
+  let string = document.getElementById("stringDecode").value;
+  let offset = parseInt(document.getElementById("offsetDecode").value);
+  let result = window.cipher.decode(offset, string); //chamando a função do cipher.js
+  document.getElementById("msgDecodificada").innerHTML = `Sua mensagem decodificada é ${result}`; 
 }
