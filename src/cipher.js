80aee45d49ce681e2a4cea5aa6a6bd8014ce32a2
campostalita <campos.talita@hotmail.com>
2019-08-30 13:20:16 -0300

    Função para decodificar número negativo ok

--- src/cipher.js
+++ src/cipher.js
@@ -2,18 +2,26 @@ window.cipher = {
   encode: encode,
   decode: decode
 };
-  
+
+function offsetNegativo(offset) {
+  while (offset < 0){
+    return offset +=26;
+  }
+  return offset;
+}
+
 function encode(offset, string) {
   let textoCodificado = [];
   let size = string.length;   
+  let offNegativo = offsetNegativo(offset);
   for (let i = 0; i <size; i++) {
     const transformAsc = string[i].charCodeAt(0);
     if (transformAsc >= 65 && transformAsc <= 90) {
-      const numerador =((transformAsc - 65 + offset) % 26) + 65;
+      const numerador =((transformAsc - 65 + offNegativo) % 26) + 65;
       const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
     } else if (transformAsc >= 97 && transformAsc <= 122) {
-      const numerador = ((transformAsc - 97 + offset) % 26) + 97;
+      const numerador = ((transformAsc - 97 + offNegativo) % 26) + 97;
       const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
     } else {
@@ -26,14 +34,15 @@ function encode(offset, string) {
 function decode(offset, string) {
   let textoCodificado = [];
   let size = string.length;
+  let offNegativoDecode = offsetNegativo(offset);
   for (let i = 0; i <size; i++) {
     const transformAsc = string[i].charCodeAt(0);
     if (transformAsc >= 65 && transformAsc <= 90) {
-      const numerador =((transformAsc - 90 - offset) % 26) + 90;
+      const numerador =((transformAsc - 90 - offNegativoDecode) % 26) + 90;
       const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
     } else if (transformAsc >= 97 && transformAsc <= 122) {
-      const numerador = ((transformAsc - 122 - offset) % 26) + 122;
+      const numerador = ((transformAsc - 122 - offNegativoDecode) % 26) + 122;
       const sCharCode = String.fromCharCode(numerador);
       textoCodificado.push(sCharCode);
     } else {
